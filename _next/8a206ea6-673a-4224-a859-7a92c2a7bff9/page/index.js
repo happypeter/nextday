e595f6544a3d3f5080b28abdce9c399fda8f34c2
Peter Wang <happypeter1983@gmail.com>
2017-08-28 16:52:36 +0800

    i

---
+++ _next/8a206ea6-673a-4224-a859-7a92c2a7bff9/page/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/', function() {
+            var comp = module.exports=webpackJsonp([5],{361:function(e,t,l){e.exports=l(362)},362:function(e,t,l){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var a=l(8),u=n(a),d=l(114),r=n(d),i=l(176),f=n(i),o=function(e){return u.default.createElement("li",null,u.default.createElement(f.default,{as:"/nextday/p/"+e.id,href:"/nextday/post?title="+e.title},u.default.createElement("a",null,e.title)))};t.default=function(){return u.default.createElement(r.default,null,u.default.createElement("h1",null,"My Blog"),u.default.createElement("ul",null,u.default.createElement(o,{id:"hello-nextjs",title:"Hello Next.js"}),u.default.createElement(o,{id:"learn-nextjs",title:"Learn Next.js is awesome"}),u.default.createElement(o,{id:"deploy-nextjs",title:"Deploy apps with Zeit"})))}}},[361]);
+            return { page: comp.default }
+          })
+        
